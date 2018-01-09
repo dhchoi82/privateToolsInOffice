5841823c7ee75bd161196e48fdf4085633c4de8f
DH.Choi <dhchoi82@nie.re.kr>
2018-01-09 13:24:02 +0900

    lineSort 프로그램에서 raw.txt 파일이 없을 경우 raw.txt 파일 생성 여부를 묻는 창 설정

--- lineSort/run.py
+++ lineSort/run.py
@@ -3,11 +3,18 @@
 import codecs
 import os
 import sys
-
-if os.path.exists("raw.txt")==False:
-    sys.exit("You need raw.txt.")
+from tkinter import Tk, messagebox
 
 text = ""
+
+if os.path.exists("raw.txt")==False:
+    root = Tk()
+    root.withdraw()
+    if messagebox.askyesno(message="raw.txt 파일이 없습니다.\nraw.txt 파일을 만드시겠습니까?"):
+        with codecs.open("raw.txt","w",encoding="utf8") as f:
+            f.write(text)
+    
+    sys.exit("You need lines in raw.txt.")
 
 with codecs.open("raw.txt","r",encoding="utf8") as f:
     text = f.read()
