ad633f70c83bc38856de635f015a8fb75c5537ad
DH.Choi <dhchoi82@nie.re.kr>
2018-01-11 12:33:08 +0900

    dataPiler/init.py 의 기능을 함수로 묶음

--- dataPiler/init.py
+++ dataPiler/init.py
@@ -2,7 +2,11 @@
 
 import sqlite3
 
-with sqlite3.connect('data.sqlite') as con, open('init.sql','r') as f:
-    cur = con.cursor()
-    sql = f.read()
-    con.executescript(sql)
+def initDB():
+    with sqlite3.connect('data.sqlite') as con, open('init.sql','r') as f:
+        cur = con.cursor()
+        sql = f.read()
+        con.executescript(sql)
+
+if __name__ == "__main__":
+    initDB()
