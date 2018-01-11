5241f288ab24b98534a9cee64197c88cae98156f
DH.Choi <dhchoi82@nie.re.kr>
2018-01-11 18:46:32 +0900

    dataPiler의 출처 테이블 field 이름 수정

--- dataPiler/init.sql
+++ dataPiler/init.sql
@@ -1,22 +1,22 @@
 BEGIN TRANSACTION;
 CREATE TABLE IF NOT EXISTS `sources` (
-	`no`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	`author`	TEXT,
-	`year`	INTEGER,
-	`title`	TEXT,
-	`journal,publisher`	TEXT,
-	`pages`	TEXT,
-	`url`	INTEGER,
-	`etc`	INTEGER
+    `no`    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    `author`    TEXT,
+    `year`  INTEGER,
+    `title` TEXT,
+    `publisher` TEXT,
+    `pages` TEXT,
+    `url`   INTEGER,
+    `etc`   INTEGER
 );
 CREATE TABLE IF NOT EXISTS `sorts` (
-	`no`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	`field`	TEXT
+    `no`    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    `field` TEXT
 );
 CREATE TABLE IF NOT EXISTS `data` (
-	`no`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	`sort`	INTEGER,
-	`datum`	TEXT,
-	`source`	INTEGER
+    `no`    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    `sort`  INTEGER,
+    `datum` TEXT,
+    `source`    INTEGER
 );
 COMMIT;
