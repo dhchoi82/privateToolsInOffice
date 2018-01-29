5ee5dc787089832a80d7534929943175161ea820
DH.Choi <dongheegyrong@hanmail.net>
2018-01-29 22:26:16 +0900

    plantNameSummary 요약 기능 개선
    분류군 수 통계 추가
    인코딩 버그 수정
    종 목록 정렬 규칙 추가

--- plantNameSummary/getList.sql
+++ plantNameSummary/getList.sql
@@ -1,4 +1,4 @@
-SELECT familyList.scientific AS familyName,
+SELECT DISTINCT familyList.scientific AS familyName,
     familyList.korean AS familyKorean,
     genusList.name AS genusName,
     specificEpithet,
@@ -12,4 +12,6 @@ SELECT familyList.scientific AS familyName,
         LEFT JOIN genusList ON genusList.id=speciesList.genusId
         LEFT JOIN familyList ON familyList.id=genusList.familyId
         LEFT JOIN intraSpecificClass ON intraSpecificClass.id=speciesList.intraspecificClassId
-    ORDER BY familyList.orderNo ASC, genusList.name ASC;
+    ORDER BY familyList.orderNo ASC, genusList.name ASC,
+        specificEpithet ASC, intraSpecificClass.classText ASC,
+        intraspecificName ASC;
--- plantNameSummary/summary.py
+++ plantNameSummary/summary.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
-import csv
+import csv, codecs
 from controlDB import getList
 
 ## 함수: makeCSV, 입력값: (종 리스트,출력파일 이름), 반환값: csv 파일 내용 문자열
 def makeCSV(fetchedList,fileName='resultList.csv'):
-    with open(fileName,'w',newline='') as csvfile:
+    with codecs.open(fileName, mode='w', encoding='utf-8') as csvfile:
         head = ['familyName','familyKorean','genusName','specificEpithet',
             'intraClass','intraspecificName','specificKorean','authorSpecific',
             'authorIntraspecific']
@@ -14,8 +14,32 @@ def makeCSV(fetchedList,fileName='resultList.csv'):
         writerObject.writerow(head)
         writerObject.writerows(fetchedList)
     
-    with open(fileName,'r') as csvfile:
+    with codecs.open(fileName, mode='r', encoding='utf-8') as csvfile:
         return csvfile.read()
 
+def makeCounts(summaryList):
+    taxaNum = len(summaryList)
+    subspNum = 0
+    varNum = 0
+    forNum = 0
+    
+    family = set()
+    genus = set()
+    
+    for item in summaryList:
+        family.add(item[0])
+        genus.add(item[2])
+        if item[4] == 'subsp.': subspNum += 1
+        elif item[4] == 'var.': varNum += 1
+        elif item[4] == 'f.': forNum += 1
+    
+    spNum = taxaNum - (subspNum + varNum + forNum)
+    input("{}과 {}속 {}종 {}아종 {}변종 {}품종 {}분류군".format(
+        len(family), len(genus), spNum, subspNum, varNum, forNum, taxaNum
+        ))
+
 if __name__ == '__main__':
-    print(makeCSV(getList()))
+    summaryList = getList()
+    print(makeCSV(summaryList))
+    makeCounts(summaryList)
+
